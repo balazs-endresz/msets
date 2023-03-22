bc3b9338e7237fdaeeeaea0bf56d58c33d1b2e63
Balazs Endresz <balazs.endresz@gmail.com>
2023-03-22 13:23:52 +0100

    RebindableSyntax improvements

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,24 @@
 {-# LANGUAGE GHC2021          #-}
-{-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE LexicalNegation  #-}
+{-# LANGUAGE OverloadedLists  #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 -- This module uses RebindableSyntax, which makes numeric literals and lists
 -- have an mset type by default. This way type annotations are usually not necessary.
 
 module Main where
+import GHC.Exts (toList)
 import Prelude qualified (fromInteger, (^), (^^))
 import Prelude hiding (fromInteger, (^), (^^))
 import Msets
 import Tests
 
+-- Numeric literals are msets by default in this file, so we get 0::Int some other way.
+-- Apply succ/pred to izero to get other numbers.
 izero = fromIntegral (maxBound - maxBound :: Int)
--- succ/pred izero to get other numbers
 
+-- msets-specific replacements for typeclass methods to make RebindableSyntax work:
 fromList :: [Mset a] -> Mset (Mset a)
 fromList (x:xs) = Cons x (fromList xs)
 fromList _      = Zero
@@ -23,13 +28,22 @@ fromInteger :: Integer -> Mset (Mset a)
 fromInteger n | n >= izero = fromList $ replicate (Prelude.fromInteger n) Zero
               | n <  izero = neg $ fromInteger -n
 
-x ^^ y = (Prelude.^^) x (toInteger @IntM y)
 x ^  y = (Prelude.^ ) x (toInteger @IntM y)
+x ^^ y = (Prelude.^^) x (toInteger @IntM y)
 
+-- values have Mset types by default without type signatures
 x0 = 0
 x1 = [1]
 x2 = [[2]]
 
+-- list syntax pattern matching works for msets too
 sqroot [[x,y]] | x == y = [[x]]
+
+-- We have NoMonomorphismRestriction here to allow pointfree functions
+-- without explicit type signatures. (This might change later)
+-- ghci> showAlpha' $ d $ 2*α^3
+-- 6α²
+-- d :: IsMset a => Mset (Mset a) -> Mset (Mset a)
+d = sum . fmap (\x -> upcast x * [x-1])
 
 main = tests
